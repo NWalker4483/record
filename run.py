5a406d1fc9190ee4b6b1002e8d4e69b3d5a7c9a2
Nile Walker <niwal7@morgan.edu>
2021-02-14 23:14:44 -0500

    sfvsa

--- run.py
+++ run.py
@@ -8,17 +8,18 @@ class Recorder(object):
     Records in mono by default.
     '''
 
-    def __init__(self, channels=1, rate=44100, frames_per_buffer=1024):
+    def __init__(self, channels=1, rate=44100, frames_per_buffer=1024, input_device_index = 0):
         self.channels = channels
         self.rate = rate
+        self.dev_index = input_device_index
         self.frames_per_buffer = frames_per_buffer
 
     def open(self, fname, mode='wb'):
-        return RecordingFile(fname, mode, self.channels, self.rate,
+        return RecordingFile(fname, mode, self.dev_index, self.channels, self.rate,
                             self.frames_per_buffer)
 
 class RecordingFile(object):
-    def __init__(self, fname, mode, channels, 
+    def __init__(self, fname, mode, dev_index, channels, 
                 rate, frames_per_buffer):
         self.fname = fname
         self.mode = mode
@@ -26,6 +27,7 @@ class RecordingFile(object):
         self.rate = rate
         self.frames_per_buffer = frames_per_buffer
         self._pa = pyaudio.PyAudio()
+        self.dev_index = dev_index
         self.wavefile = self._prepare_file(self.fname, self.mode)
         self._stream = None
 
@@ -54,6 +56,7 @@ class RecordingFile(object):
                                         rate=self.rate,
                                         input=True,
                                         frames_per_buffer=self.frames_per_buffer,
+                                        input_device_index = self.dev_index,
                                         stream_callback=self.get_callback())
         self._stream.start_stream()
         return self
@@ -83,7 +86,7 @@ class RecordingFile(object):
 
 app = Flask(__name__)
 
-rec = Recorder(channels=1)
+rec = Recorder(channels=2,input_device_index = dev_index)
 open_file = None 
 fileList = ["non-non-blocking.wav", "non-blocking.wav", "blocking.wav"]
 @app.route('/', methods=['GET', 'POST'])
@@ -99,8 +102,8 @@ def hello_world():
             print('Stopped Recording')
         if request.form['button'] == 'off':
             state, name = "on", "Stop Recording"
-            # open_file = rec.open('nonblocking.wav', 'wb')
-            # open_file.start_recording()
+            open_file = rec.open('nonblocking.wav', 'wb')
+            open_file.start_recording()
             print('Started Recording')
     return render_template("index.html",state=state, name=name, fileList=fileList)
 if __name__ == '__main__':
